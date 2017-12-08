8161b65ec5b954022f3777747d509c5add9caeb0
Tho Cao <thocao92@gmail.com>
2017-12-08 12:32:02 +0900

    Function For Capture is Donw

--- Stop Advertisement/CaptureScreen.h
+++ Stop Advertisement/CaptureScreen.h
@@ -1,8 +1,19 @@
 #pragma once
+
+#include <opencv2\opencv.hpp>
+#include <Windows.h>
+#include <iostream>
+
 class CaptureScreen
 {
 public:
 	CaptureScreen();
 	~CaptureScreen();
+
+	cv::Mat getImageFromScreenPC(void);
+private:
+
+	HWND hwndDesktop;
+
 };
 
